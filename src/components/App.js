70cb6532c5563f054bc917a48f39f9742f024124
Cooper Sawyer <cooper.sawyer@powerhrg.com>
2021-07-15 11:43:21 -0400

    almost done with 3rd deliverable

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [sushiList, setSushiList] = useState([])
+
+
+  useEffect(() => {
+    fetch(API)
+    .then(response => response.json())
+    .then(data => setSushiList(data))
+  }, [])
+
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer 
+      sushiList={sushiList}
+      />
       <Table />
     </div>
   );
--- src/components/MoreButton.js
+++ src/components/MoreButton.js
@@ -1,7 +1,8 @@
 import React from "react";
 
-function MoreButton(props) {
-  return <button onClick={/* Fill me in! */ null}>More sushi!</button>;
+function MoreButton({ onCLickMore }) {
+
+  return <button onClick={(event) => onClickMore(event.target.clicked)} >More sushi!</button>;
 }
 
 export default MoreButton;
